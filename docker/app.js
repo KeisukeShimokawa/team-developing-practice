a6231b35ce22aae9a5e9ea124006b652639e569b
KeisukeShimokawa <shimoke4869@gmail.com>
2021-01-15 11:40:03 +0000

    docs: 動作検証した結果M1MacでDockerの起動は問題なし #18

--- docker/app.js
+++ docker/app.js
@@ -1,15 +1,46 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const app = express();
 
+app.use(cookieParser());
 app.use("/public", express.static(path.resolve(__dirname + "/public")));
 
 app.get("/", (req, res) => {
   console.log(req.headers);
+
+  console.log(req.cookies);
+
+  res.cookie("key", "value", {
+    maxAge: 60000,
+    httpOnly: false
+  })
+
   res.status(200);
   res.json({
-    message: "root access succeed"
+    message: "root access succeed",
+    cookies: req.cookies
   })
 });
+
+app.get("/get", (req, res) => {
+  console.log(req.headers);
+  console.log(req.cookies);
+
+  res.status(200);
+  res.json({
+    message: "fetch get"
+  })
+})
+
+app.post("/post", (req, res) => {
+  console.log(req.headers);
+  console.log(req.cookies);
+
+  res.status(200);
+  res.json({
+    message: "fetch post"
+  })
+})
 
 app.listen(8080);
